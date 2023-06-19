83d54fe401695e647fd54ce90261af782db6eb1f
apriliyanto-pambudi <apriliyanto.pambudi@gmail.com>
2023-06-19 23:33:38 +0700

    website movie

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { getMovieList, searchMovie } from './Api';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [popularMovies, setPopularMovies] = useState([]);
+
+	useEffect(() => {
+		getMovieList().then((result) => {
+			setPopularMovies(result);
+		});
+	}, []);
+
+	const PopularMovieList = () => {
+		return popularMovies.map((movie, i) => {
+			return (
+				<div className='Movie-wrapper' key={i}>
+					<div className='Movie-title'>{movie.title}</div>
+					<img
+						className='Movie-image'
+						src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}
+						alt=''
+					/>
+					<div className='Movie-date'>{movie.release_date}</div>
+					<div className='Movie-rate'>Ratings : {movie.vote_average}</div>
+				</div>
+			);
+		});
+	};
+
+	const search = async (q) => {
+		if (q.length > 3) {
+			const query = await searchMovie(q);
+			setPopularMovies(query.results);
+		}
+	};
+
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<h1>MOVIE TIME</h1>
+				<input
+					placeholder='Search Movie...'
+					className='Movie-search'
+					onChange={({ target }) => search(target.value)}
+				/>
+				<div className='Movie-container'>
+					<PopularMovieList />
+				</div>
+			</header>
+		</div>
+	);
+};
 
 export default App;
